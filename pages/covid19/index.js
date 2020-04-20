d5eaad2b4227c8a282c6f0110ede9f1135f9d054
mwangiKibui <kenmwangi250@gmai.com>
2020-04-20 11:20:20 +0300

    changed covid19 card image

--- pages/covid19/index.js
+++ pages/covid19/index.js
@@ -15,7 +15,7 @@ const Index = (props) =>  {
         title="Corona Virus Outbreak. | allcomrades cares"
         description="Get realtime data of infected,recovered and death cases resulting from covid19 from all over the world"
         url="https://allcomrades.co.ke/covid19"
-        image="/static/images/nba.webp"
+        image="https://karu-api.s3.amazonaws.com/adverts/1587370699472.jpg"
         />
         <section className="covid19">
             <div className="container">
